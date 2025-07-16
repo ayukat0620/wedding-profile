a4e568d3939972ccf8461d6fd35e75a204f0503d
ayukat <takuya.12191205@gmail.com>
2025-07-16 21:43:02 +0900

    humberger

--- script.js
+++ script.js
@@ -44,3 +44,9 @@ modals.forEach(modal => {
   });
 });
 
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
